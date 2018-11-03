412d557a8d241b971bc13a4663743d04fa2f96ec
Inderjeet Kaur <inderjeet.kchawla@gmail.com>
2018-11-03 10:18:19 -0400

    install cors

--- ProxyServer/server.js
+++ ProxyServer/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const parser = require('body-parser');
+const cors = require('cors');
 const port = process.env.PORT || 8080;
 const axios = require('axios');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(parser.json());
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
